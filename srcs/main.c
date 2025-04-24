251fc75064a441977b273d2d9a065c1e4c231327
binR <raph.biner@gmail.com>
2025-04-24 14:30:35 +0200

    start

---
+++ srcs/main.c
@@ -0,0 +1,15 @@
+#include "pipex.h"
+
+int	main(int ac, char **av)
+{
+	if (ac != 3)
+	{
+		ft_printf("Error\nac != 3\n");
+		return (0);
+	}
+
+	t_pipes	test;
+	
+
+	return (0);
+}
---
+++ srcs/pipex.h
@@ -0,0 +1,21 @@
+#ifndef PIPEX_H
+# define PIPEX_H
+
+//# include <unistd.h>
+//# include <stdlib.h>
+
+//# include <stdio.h>
+
+typedef struct s_pipes
+{
+	int	i;
+	int	j;
+}		t_pipes;
+
+// main.c
+
+
+// pipex.c
+
+
+#endif
